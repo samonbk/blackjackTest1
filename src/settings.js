4e404340e26e5465e9a38293d90f47e50b4ce1fd
samonbk <136555363+samonbk@users.noreply.github.com>
2023-07-27 16:47:50 +0700

    Add files via upload

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/menu.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: []
+};
